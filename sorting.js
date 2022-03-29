408e00a888f1191b0dd73af3e88680642e47f572
deepak91144 <deepak_dibyajyoti@hotmail.com>
2022-03-29 19:14:22 +0530

    added quicksort and mergesort

--- sorting.js
+++ sorting.js
@@ -44,6 +44,95 @@ const selectionSort = () => {
   }
   console.log(arr);
 };
+
+// approach 3 quicksort
+
+const partition = (A, low, high) => {
+  let pivot = A[low];
+  let i = low + 1;
+  let j = high;
+  let temp;
+
+  do {
+    while (A[i] <= pivot) {
+      i++;
+    }
+
+    while (A[j] > pivot) {
+      j--;
+    }
+
+    if (i < j) {
+      temp = A[i];
+      A[i] = A[j];
+      A[j] = temp;
+    }
+  } while (i < j);
+
+  // Swap A[low] and A[j]
+  temp = A[low];
+  A[low] = A[j];
+  A[j] = temp;
+  return j;
+};
+const quickSort = (arr, low, high) => {
+  let partitionIndex;
+  if (low < high) {
+    partitionIndex = partition(arr, low, high);
+    quickSort(arr, low, partitionIndex - 1);
+    quickSort(arr, partitionIndex + 1, high);
+  }
+  console.log(arr);
+};
+// approach 5 merge sort function
+const merge = (A, mid, low, high) => {
+  let i,
+    j,
+    k,
+    B = [];
+  i = low;
+  j = mid + 1;
+  k = low;
+
+  while (i <= mid && j <= high) {
+    if (A[i] < A[j]) {
+      B[k] = A[i];
+      i++;
+      k++;
+    } else {
+      B[k] = A[j];
+      j++;
+      k++;
+    }
+  }
+  while (i <= mid) {
+    B[k] = A[i];
+    k++;
+    i++;
+  }
+  while (j <= high) {
+    B[k] = A[j];
+    k++;
+    j++;
+  }
+  for (let i = low; i <= high; i++) {
+    A[i] = B[i];
+  }
+};
+const mergeSort = (A, low, high) => {
+  let mid;
+  if (low < high) {
+    mid = (low + high) / 2;
+    mergeSort(A, low, mid);
+    mergeSort(A, mid + 1, high);
+    merge(A, mid, low, high);
+  }
+  console.log(A);
+};
+// arpporoach 6 count sorting
+
 // insertionSort();
 // bubbleSort();
-selectionSort();
+// selectionSort();
+// mergeSort(arr, 0, arr.length - 1);
+quickSort(arr, 0, arr.length - 1);
